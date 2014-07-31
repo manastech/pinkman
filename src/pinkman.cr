1fcccb77d3ca28fefe7a4ae749c4d613e2692795
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-07-31 01:27:17 -0300

    Updated with OpenSSL refactor in crystal

--- src/pinkman.cr
+++ src/pinkman.cr
@@ -1,7 +1,8 @@
 require "frank"
+require "openssl"
 
 Frank.config do |config|
-  config.ssl = SSLContext.new.tap do |ssl_ctx|
+  config.ssl = OpenSSL::SSL::Context.new.tap do |ssl_ctx|
     ssl_ctx.certificate_chain = "server.pem"
     ssl_ctx.private_key = "server.key"
   end
